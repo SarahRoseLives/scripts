199dc77de38f63d6c8e00460a1f1a8bcd20259a6
Sarah Rose <156587765+SarahRoseLives@users.noreply.github.com>
2024-07-11 23:39:41 -0400

    Create install_rtl_v4.sh

---
+++ install_rtl_v4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Purge the previous driver
+sudo apt purge ^librtlsdr
+sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr* /usr/local/include/rtl_* /usr/local/bin/rtl_*
+
+# Install the RTL-SDR Blog drivers
+sudo apt-get install libusb-1.0-0-dev git cmake pkg-config -y
+git clone https://github.com/rtlsdrblog/rtl-sdr-blog
+cd rtl-sdr-blog
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo ldconfig
+
+# Blacklist the DVB-T TV drivers
+echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
+
+# Reboot
+sudo reboot
